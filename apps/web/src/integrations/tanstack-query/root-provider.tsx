17a1e098adf5da9af8b5954edc4401b487aea5a0
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-12-22 21:09:25 -0300

    :tada: feat: create app web

---
+++ apps/web/src/integrations/tanstack-query/root-provider.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { ReactNode } from 'react'
+
+export function getContext () {
+  const queryClient = new QueryClient()
+  return {
+    queryClient,
+  }
+}
+
+export function Provider ({
+  children,
+  queryClient,
+}: {
+  children: ReactNode
+  queryClient: QueryClient
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
